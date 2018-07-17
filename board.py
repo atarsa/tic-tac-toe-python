eb9d0c79fb1285e1cc1803deb1686a173154d7c1
ATarsa <smb909@gmail.com>
2018-07-17 13:11:47 +0100

    Added functions to create a board

---
+++ board.py
@@ -0,0 +1,19 @@
+def draw_roof(tiles):
+    roof = " " + "-"*3
+    print(roof*tiles)
+
+
+def draw_wall(tiles):
+    print("|   "*(tiles+1))
+
+
+def draw_row(rows):
+    for row in range(rows):
+        draw_roof(rows)
+        draw_wall(rows)
+    draw_roof(rows)
+
+
+size = input("What size game board do you want to draw? ")
+
+draw_row(int(size))
